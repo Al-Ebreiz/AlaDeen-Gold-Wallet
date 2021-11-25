8f4aaf576abc1be6804cad677424b0318e6fff07
Al Ebreiz <al.ebreiz.global.capital.berhad@gmail.com>
2021-11-25 15:23:03 +0800

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route  } from 'react-router-dom';
 import App from './App';
+import Terms from './routes/Terms';
+import Privacy from './routes/Privacy';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const rootElement = document.getElementById("root");
+render(
+  <Router>
+    <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="Terms" element={<Terms />} />
+        <Route path="Privacy" element={<Privacy />} />
+    </Routes>
+  </Router>,
+  rootElement
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//    <BrowserRouter>
+//     <Routes>
+//       <Route path="/" element={<App />} />
+//       <Route path="expenses" element={<Terms />} />
+//       <Route path="invoices" element={<Privacy />} />
+//     </Routes>
+//   </BrowserRouter>,
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
